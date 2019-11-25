1600800d95fc4c940dc5818803edf14ece58d0f2
shaohlalnyd <55618167+shaohlalnyd@users.noreply.github.com>
2019-11-25 10:58:01 +0330

    shohlalnyd

--- src/app/sale-module/sale/sale.component.ts
+++ src/app/sale-module/sale/sale.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {$} from '../../../assets/jquery.min.js'
 import { NgxSmartModalService } from 'ngx-smart-modal';
 declare var $ :any
@@ -8,10 +8,17 @@ declare var $ :any
   styleUrls: ['./sale.component.scss']
 })
 export class SaleComponent implements OnInit {
+  adWidth: number
+  @HostListener("window:resize")
+  onResize() {
+      this.adWidth = document.getElementsByClassName('ad')[0].clientWidth
+  }
 
   constructor(public ngxSmartModalService : NgxSmartModalService) { }
   ngOnInit() {
-
+    setTimeout(() => {
+      this.adWidth=document.getElementsByClassName('ad')[0].clientWidth
+    }, 100);
   }
   filterHandler(){
 
